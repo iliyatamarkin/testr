7314d5731fb85062f38742eb9c33d01ae6261a96
iliya <iliya.tamarkin@gmail.com>
2019-10-13 00:44:00 +0300

    moved css to js files for graph summery

--- testr/src/HomePage/Dashboard/GraphSummery/GraphSummery.js
+++ testr/src/HomePage/Dashboard/GraphSummery/GraphSummery.js
@@ -1,45 +1,103 @@
 import React from 'react';
-import './GraphSummery.css';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-export default class GraphSummary extends React.Component {
-  graphColorsMap = {
+const useStyles = makeStyles(theme => ({
+  PieChart: {
+    width: '54.5px',
+    height: '54.5px',
+    'object-fit': 'contain',
+  },
+
+  Graph: {
+    width: '135px',
+    height: '50.5px',
+    'object-fit': 'contain',
+  },
+
+  Title: {
+    'font-family': 'Lato',
+    'font-size': '7.5px',
+    'font-weight': 'bold',
+    'font-style': 'normal',
+    'font-stretch': 'normal',
+    'line-height': 'normal',
+    'letter-spacing': 'normal',
+    color: '#354052',
+  },
+  Followers: {
+    'font-family': 'Lato',
+    'font-size': '7.5px',
+    'font-weight': 'normal',
+    'font-style': 'normal',
+    'font-stretch': 'normal',
+    'line-height': '1.93',
+    'letter-spacing': '0.05px',
+    color: '#8a96a0',
+    display: 'inline-block',
+  },
+  Number: {
+    width: '34px',
+    height: '16px',
+    'font-family': 'Helvetica',
+    'font-size': '13.5px',
+    'font-weight': 'normal',
+    'font-style': 'normal',
+    'font-stretch': 'normal',
+    'line-height': 'normal',
+    'letter-spacing': 'normal',
+    ' text-align': 'center',
+    color: '#37394c',
+    display: 'inline-block',
+  },
+
+  Line: {
+    border: 'solid 0.5px #f0f0f0',
+  },
+}));
+
+const GraphSummary = props => {
+  const graphColorsMap = {
     'Ratings by category': 'pink',
     'General results': 'blue',
   };
 
-  render() {
-    const title = this.props.type;
-    const color = this.graphColorsMap[this.props.type];
+  const classes = useStyles();
 
-    return (
-      <Grid
-        container
-        justify="center"
-        direction="row"
-        className="Line"
-        alignItems="center"
-      >
+  const title = props.type;
+  const color = graphColorsMap[props.type];
+
+  return (
+    <Grid
+      container
+      justify="center"
+      direction="row"
+      className={classes.Line}
+      alignItems="center"
+    >
+      <Grid item xs={12}>
+        <div className={classes.Title}>{title}</div>
+      </Grid>
+      <Grid item container spacing={1} xs={8}>
+        <Grid item xs={7}>
+          <div className={classes.Number}>9401</div>
+          <div className={classes.Followers}>Followers</div>
+        </Grid>
         <Grid item xs={12}>
-          <div className="Title">{title}</div>
-        </Grid>
-        <Grid item container spacing={1} xs={8}>
-          <Grid item xs={7}>
-            <div className="Number">9401</div>
-            <div className="Followers">Followers</div>
-          </Grid>
-          <Grid item xs={12}>
-            <img src={color + '_charts/graph.svg'} className="Graph"></img>
-          </Grid>
-        </Grid>
-        <Grid item xs={3}>
           <img
-            src={color + '_charts/pie_chart.svg'}
-            className="Pie-chart"
+            src={color + '_charts/graph.svg'}
+            className={classes.Graph}
           ></img>
         </Grid>
       </Grid>
-    );
-  }
-}
+      <Grid item xs={3}>
+        <img
+          src={color + '_charts/pie_chart.svg'}
+          className={classes.PieChart}
+        ></img>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default GraphSummary;
